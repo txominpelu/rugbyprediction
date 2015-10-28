0f9bafdff560f02acdd01b1b38e5aa901df9610c
Inigo Mediavilla <imediava@gmail.com>
2015-10-28 23:04:53 +0100

    Using data preprocessed by spark job

--- machine_learning.py
+++ machine_learning.py
@@ -5,30 +5,17 @@ from sklearn.ensemble import RandomForestClassifier
 import random
 from sklearn import tree
 from sklearn import cross_validation
+import numpy
 
-df = pandas.read_csv('data/ground_matches.csv')
-df['team_last10_avg_diff'].fillna(0, inplace=True)
-df['rival_last10_avg_diff'].fillna(0, inplace=True)
-df['team_last10_avg_same_match_diff'].fillna(0, inplace=True)
-df['team_last10_avg_reverse_match_diff'].fillna(0, inplace=True)
-
-def sample(percent):
-    rows = random.sample(df.index, percent)
-    df_30 = df.ix[rows]
-    df_70 = df.drop(rows)
-    return (df_30, df_70)
-
-def draw_decission_tree():
-    clf2 = tree.DecisionTreeClassifier()
-    tree.export_graphviz(clf2, out_file='tree.dot')
-
-
-df_30, df_70 = sample(30)
+df = pandas.read_csv('data/matches-spark3.csv',delimiter=";")
+df = df.convert_objects(convert_numeric=True)
+df.fillna(0, inplace=True)
 
 def see_prediction_precision(result_column):
     clf = RandomForestClassifier(n_estimators=10)
-    consider_columns = ['team_last10_avg_diff','rival_last10_avg_diff','home_match','team_last10_avg_same_match_diff', 'team_last10_avg_reverse_match_diff'] 
-    #consider_columns = ['team_last10_avg_diff','rival_last10_avg_diff','home_match'] 
+    exclude = ["diff","team_score","rival_score"]
+    consider_columns = [k for (k,v) in df.dtypes.to_dict().items() if k <> result_column and not (k in exclude) and v.type in [numpy.int64, numpy.float64, numpy.bool_]]
+    print "Using columns: {0}".format(consider_columns)
     target = df[result_column]
     data = df[consider_columns]
     for i in range(0,4):
@@ -37,14 +24,8 @@ def see_prediction_precision(result_column):
         print "Percentage:"
         print clf.score(X_test, y_test)
 
-	#print "Failed at:"
-	#print "Tested for:"
-	#prediction = clf.predict(X_test)
-	#X_test['result'] = y_test
-	#X_test['prediction'] = prediction
-	#print X_test
 
-print "Prediction b_match_diff"
-see_prediction_precision('b_match_diff')
+#print "Prediction b_match_diff"
+#see_prediction_precision('b_match_diff')
 print "Prediction result"
 see_prediction_precision('result')
--- myspider.py
+++ myspider.py
@@ -1,18 +1,25 @@
 import scrapy
 import re
+import urllib2
+import json
 
 
 class MatchItem(scrapy.Item):
     result  = scrapy.Field() 
-    team_score = scrapy.Field()
-    rival_score = scrapy.Field()
     # diff
     htf = scrapy.Field()
     hta = scrapy.Field()
-    rival_name = scrapy.Field()
     ground = scrapy.Field()
     date = scrapy.Field()
-    team_id = scrapy.Field()
+    gameId = scrapy.Field()
+    league = scrapy.Field()
+    home_name = scrapy.Field()
+    home_id = scrapy.Field()
+    home_score = scrapy.Field()
+    away_name = scrapy.Field()
+    away_score = scrapy.Field()
+    away_id = scrapy.Field()
+    competition_name = scrapy.Field()
  
 
 class BlogSpider(scrapy.Spider):
@@ -27,13 +34,33 @@ class BlogSpider(scrapy.Spider):
         for site in sites:
             item = MatchItem()
             item['result'] = site.xpath('td[1]/text()').extract()
-            item['team_score'] = site.xpath('td[2]/text()').extract()
-            item['rival_score'] = site.xpath('td[3]/text()').extract()
-            item['htf'] = site.xpath('td[5]/text()').extract()
-            item['hta'] = site.xpath('td[6]/text()').extract()
-            item['rival_name'] = site.xpath('td[8]/text()').extract()
-            item['ground'] = site.xpath('td[9]/a/text()').extract()
-            item['date'] = site.xpath('td[10]/b/text()').extract()
-            item['team_id'] = re.search('http://en.espn.co.uk/statsguru/rugby/team/{0}.html'.replace("{0}",'(?P<teamid>\d*)'), response.url).group('teamid')
-            yield item
+            item['htf'] = site.xpath('td[5]/text()').extract()[0]
+            item['hta'] = site.xpath('td[6]/text()').extract()[0]
+            item['ground'] = site.xpath('td[9]/a/text()').extract()[0]
+            item['date'] = site.xpath('td[10]/b/text()').extract()[0]
+	    match_details_url = site.xpath('td[11]/a/@href').extract()[0]
+	    yield scrapy.Request(response.urljoin(match_details_url), lambda x: self.extract_match_api_url(site, x), meta={'item':item} )
+
+    def extract_match_api_url(self, site, response):
+	item = MatchItem(response.meta["item"])
+	m = re.search("gameId=(?P<game>\d*)&league=(?P<league>\d*)",response.url)
+	url = 'http://site.api.espn.com/apis/site/v2/sports/rugby/{0}/summary?event={1}'.format(m.group('league'),m.group('game'))
+	yield scrapy.Request(url, self.parse_match, meta={'item':item} )
+
+    def parse_team(self, json, item, prefix):
+        item["{0}_name".format(prefix)] = json['team']['displayName']
+        item["{0}_id".format(prefix)] = json['team']['id']
+        item["{0}_score".format(prefix)] = json['score']
+        return item
+
+    def parse_match(self, response):
+	item = MatchItem(response.meta["item"])
+	js = json.loads(response.body_as_unicode())
+	competitors = js['header']['competitions'][0]['competitors']
+	item = self.parse_team([ t for t in competitors if t['homeAway'] == 'home'][0], item, "home")
+	item = self.parse_team([ t for t in competitors if t['homeAway'] != 'home'][0], item, "away")
+	item['gameId'] = js['header']['id']
+	item['league'] = js['header']['league']['id']
+	item['competition_name'] = js['header']['league']['abbreviation']
+	yield item
 
